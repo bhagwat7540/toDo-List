fe4ffff387aea5c522c47ab4ca3a0e959b9d106b
bhagwat7540 <bhagwatmiglani@gmail.com>
2022-05-22 02:35:46 +0530

    done

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = ["cook", "buy"];
+let workItems = [];
+
+app.get("/", function (req, res) {
+  // console.log(items);
+  const day = date();
+
+  res.render("list", { listTitle: day, listOfItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+
+  if (req.body.list == "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    console.log(req.body);
+
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", listOfItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server running on port 3000");
+});
